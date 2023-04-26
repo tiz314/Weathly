9ebe35df7e9437aafa9e816e27fb0b048a3b5208
Tiz314 <63679072+tiz314@users.noreply.github.com>
2023-04-26 21:19:34 +0200

    update

---
+++ src/assets/scripts/lang.js
@@ -0,0 +1,33 @@
+const lang = {
+
+    en: {
+        title: `<span class="highlightTitle">Open</span> and <span class="highlightTitle">real-time</span> weather data`,
+        searchBox: "Milan, IT",
+        about: "About",
+        settings: "Settings",
+        log: "Login or Signup",
+        search: "Search",
+        fork: "Fork on",
+        openData: "Open data from",
+        aboutMessage: "Wealthy is an open source project created for a computer science class, designed to provide a user-friendly way to access real-time weather data from OpenWeather. The focus is on front-end development tools, particularly JavaScript. The app includes features to enhance the user experience and is valuable for anyone who wants to stay informed about weather conditions worldwide.",
+        notFound: "City not found!",
+        feelsLike: "Feels like"
+    },
+
+    it: {
+        title: `Informazioni metereologiche in <span class="highlightTitle">tempo reale</span>`,
+        searchBox: "Milano, IT",
+        about: "Chi siamo",
+        settings: "Impostazioni",
+        log: "Accedi o registrati",
+        search: "Cerca",
+        fork: "Esplora",
+        openData: "Dati offerti da",
+        aboutMessage: "Wealthy è un progetto open source creato per una classe di informatica, progettato per fornire un modo intuitivo per accedere ai dati meteorologici in tempo reale da OpenWeather. L'attenzione è rivolta agli strumenti di sviluppo front-end, in particolare JavaScript. L'app include funzionalità per migliorare l'esperienza dell'utente ed è preziosa per chiunque desideri rimanere informato sulle condizioni meteorologiche in tutto il mondo.",
+        notFound: "Città non trovata!",
+        feelsLike: "Percepiti"
+    }
+
+
+
+}
