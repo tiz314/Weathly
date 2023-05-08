6c61bb3469a739e155b8274f8a84f74c22c630e5
tiz314 <63679072+tiz314@users.noreply.github.com>
2023-05-08 09:30:01 +0200

    adding data

--- src/public/assets/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-let footerContent = 
-`<footer class="text-center" id="footer">
-    <div class="text-center p-3">
-        <p><b>Weathly</b>: <span id="fork">Fork on</span> <a href="https://mdbootstrap.com/">GitHub</a></p>
-        <p><span id="openData">Open data from</span><a href="https://openweathermap.org/"> OpenWeather</a></p>
-    </div>
-</footer>`;
-
-document.getElementById("footerContainer").innerHTML = footerContent;
